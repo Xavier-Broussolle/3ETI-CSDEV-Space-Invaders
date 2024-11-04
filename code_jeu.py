63d97172ccb6e67db79ddd7f695f607e93861c2c
Xavier BROUSSOLLE <xavbrsl@protonmail.com>
2024-11-04 15:00:53 +0100

    v2 XB 4nov24

---
+++ code_jeu.py
@@ -0,0 +1,35 @@
+### ------------------------------------------------- ###
+### SPACE INVADERS ###
+# UTF8
+# Auteurs : ARRIEU Matteo, BROUSSOLLE Xavier 
+# Date de creation du fichier : Lundi 4 Nov 2024
+# Derniere mise a jour : Lundi 4 Nov 2024
+
+
+### ------------------------------------------------- ###
+# creation objet bloc aliens 
+class blocAlien :
+    def __init__(self, position, vitesse):
+        self.position = position
+        self.vitesse = vitesse 
+
+# creation objet alien 
+class alien : 
+    def __init__(self, position) : 
+        self.position = position 
+
+# creation protection 
+class protection :
+    def __init__(self, position):
+        self.position = position
+
+# creation objet vaisseau 
+class vaisseau :
+    def __init__(self, position, vies):
+        self.position = position
+        self.vies = vies 
+
+# creation objet projectile 
+class projectile : 
+    def __init__(self, position):
+        self.position = position 
