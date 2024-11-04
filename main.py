60e3dc84996cfa47ff6c38f33748a05c89b9b824
Xavier BROUSSOLLE <xavbrsl@protonmail.com>
2024-11-04 17:01:26 +0100

    creation du fichier main

---
+++ main.py
@@ -0,0 +1,13 @@
+### ------------------------------------------------- ###
+### SPACE INVADERS - fichier python principal ###
+# UTF8
+# Auteurs : ARRIEU Matteo, BROUSSOLLE Xavier 
+# Date de creation du fichier : Lundi 4 Nov 2024
+# Derniere mise a jour : Lundi 4 Nov 2024
+
+### - Imports - ###
+import Affichage
+
+### - Fonction main - ###
+def main():
+    maToile = Affichage()
