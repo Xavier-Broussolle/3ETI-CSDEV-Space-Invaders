d9d654944ce33ba7a988dd2d959bca5e733284f0
Xavier BROUSSOLLE <xavbrsl@protonmail.com>
2024-11-04 17:00:18 +0100

    affichage et progression alien

--- Affichage.py
+++ Affichage.py
@@ -3,11 +3,15 @@ import tkinter as tk
 SI = tk.Tk()
 SI.title("Space Invaders")
 SI.geometry('1190x780')
+
 Titre = tk.Label(SI, text="Space Invaders", fg = 'black')
 Titre.pack(pady= 10)
+
 button_options = tk.Button(SI, text ="Options", fg='black')
 button_options.pack(pady =100)
-buttonquit = tk.Button(SI, text="Quitter", fg='red', command=SI.destroy)
+
+buttonquit = tk.Button(SI, text="Quitter", fg='red', command=SI.quit)
 buttonquit.pack(pady=50)
+
 SI.mainloop()
 
