59cf86f045f62fae2d3b5b6ecfc5ca6e45abe8b9
Xavier BROUSSOLLE <xavbrsl@protonmail.com>
2024-11-04 17:43:17 +0100

    v4.1 brouillon XB

--- code_jeu_playground.py
+++ code_jeu_playground.py
@@ -8,39 +8,49 @@
 
 ### - Librairies - ###
 import tkinter as tk
-from Affichage import SI
 
-### - Images - ###
-photoVaisseau = tk.PhotoImage(file="./images/vaisseau.png")
-image_label = tk.Label(SI, image=photoVaisseau)
-image_label.pack()
+### - Gestion affichage global - ### 
+class Toile : 
+    def __init__(self):
+        self.mf = tk.Tk()
+        self.cnv = tk.Canvas(self.mf, width=1200, height=800, bg="white")
+        self.cnv.pack()
+        #self.btn_start = tk.Button(self.mf, text="Jouer")
+        #self.btn_start.pack(padx=100, side='left')
+        self.btn_quit = tk.Button(self.mf, text="Quitter", command=self.mf.quit)
+        self.btn_quit.pack(padx=100, side='right')
+        self.mf.mainloop()
+
+toile1 = Toile()
 
 
 ### - Gestion des classes - ###
 # creation objet bloc aliens 
-class blocAlien :
+class BlocAlien :
     def __init__(self, position, vitesse):
         self.position = position
         self.vitesse = vitesse 
 
 # creation objet alien 
-class alien : 
+class Alien : 
     def __init__(self, position) : 
         self.position = position 
 
 # creation protection 
-class protection :
+class Protection :
     def __init__(self, position):
         self.position = position
 
 # creation objet vaisseau 
-class vaisseau :
-    def __init__(self, positionX, positionY, vies):
+# creation image vaisseau
+photoVaisseau = tk.PhotoImage(file="./images/vaisseau.png")
+class Vaisseau :
+    def __init__(self, positionX, positionY):
         self.positionX = positionX
         self.positionY = positionY
-        self.vies = vies 
-        self.canva = tk.Canvas(SI, width=160, height=187, bg='')
-        self.imageVaisseau = tk.create_image(0, 0, anchor="nw", file=photoVaisseau)
+        self.vies = 3 
+        self.canva = tk.Canvas(toile1, width=160, height=187, bg='')
+        self.imageVaisseau = tk.create_image(positionX, positionY, image=photoVaisseau)
         self.canva.coords(positionX, positionY, self.imageVaisseau)
         self.tk.focus_set()
 
@@ -52,7 +62,9 @@ class vaisseau :
         if touche == "d" : 
             self.positionX += 40
 
+Vaisseau(200, 200 )
+
 # creation objet projectile 
-class projectile : 
+class Projectile : 
     def __init__(self, position):
         self.position = position 
